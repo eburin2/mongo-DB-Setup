5411d76f2c8d22172805e4d926a426656065a5da
Elliot Burin <eburin2@gmail.com>
2020-04-16 16:29:10 -0700

    add User schema and model functionality and setup mocha

---
+++ test/test_helper.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/users_test');
+
+mongoose.connection
+  .once('open', () => console.log('Good to go'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
+
+  beforeEach(() => {
+    mongoose.connection.collections.users.drop();
+  });
